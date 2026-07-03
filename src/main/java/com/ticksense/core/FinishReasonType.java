b66382493ce0100ff966ffc06883ed53c5100996
Scott Rangeley <scott.rangeley@hotmail.com>
2026-07-03 11:56:13 +0100

    refactor: move finish reason display names to the enum

--- src/main/java/com/ticksense/core/FinishReasonType.java
+++ src/main/java/com/ticksense/core/FinishReasonType.java
@@ -1,5 +1,7 @@
 package com.ticksense.core;
 
+import java.util.Locale;
+
 public enum FinishReasonType
 {
     COMPLETED,
@@ -19,5 +21,21 @@ public enum FinishReasonType
     REWARD_RECEIVED,
     CLIENT_SHUTDOWN,
     AMBIGUOUS_CONTEXT_LOST,
-    UNKNOWN
+    UNKNOWN;
+
+    public String displayName()
+    {
+        final String[] words = name().split("_");
+        final StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < words.length; i++)
+        {
+            if (i > 0)
+            {
+                builder.append(' ');
+            }
+            final String word = words[i].toLowerCase(Locale.US);
+            builder.append(Character.toUpperCase(word.charAt(0))).append(word.substring(1));
+        }
+        return builder.toString();
+    }
 }

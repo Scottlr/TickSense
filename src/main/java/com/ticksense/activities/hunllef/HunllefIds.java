16724a266783be571de5c20d017cb8636504ade9
Scott Rangeley <Scottlr@users.noreply.github.com>
2026-07-03 19:32:18 +0100

    refactor: expose intent boss id checks

--- src/main/java/com/ticksense/activities/hunllef/HunllefIds.java
+++ src/main/java/com/ticksense/activities/hunllef/HunllefIds.java
@@ -1,53 +1,40 @@
 package com.ticksense.activities.hunllef;
 
+import com.ticksense.common.IntIdSet;
+
 public final class HunllefIds
 {
-    private static final int[] CRYSTALLINE_HUNLLEF_NPC_IDS = {
+    private static final IntIdSet CRYSTALLINE_HUNLLEF_NPC_IDS = IntIdSet.of(
         9021, // RuneLite NpcID.CRYSTALLINE_HUNLLEF
         9022, // RuneLite NpcID.CRYSTALLINE_HUNLLEF_9022
         9023, // RuneLite NpcID.CRYSTALLINE_HUNLLEF_9023
         9024, // RuneLite NpcID.CRYSTALLINE_HUNLLEF_9024
         12123, // RuneLite NpcID.CRYSTALLINE_HUNLLEF_12123
         15613, // RuneLite NpcID.CRYSTALLINE_HUNLLEF_15613
-        15614 // RuneLite NpcID.CRYSTALLINE_HUNLLEF_15614
-    };
+        15614); // RuneLite NpcID.CRYSTALLINE_HUNLLEF_15614
 
-    private static final int[] CORRUPTED_HUNLLEF_NPC_IDS = {
+    private static final IntIdSet CORRUPTED_HUNLLEF_NPC_IDS = IntIdSet.of(
         9035, // RuneLite NpcID.CORRUPTED_HUNLLEF
         9036, // RuneLite NpcID.CORRUPTED_HUNLLEF_9036
         9037, // RuneLite NpcID.CORRUPTED_HUNLLEF_9037
-        9038 // RuneLite NpcID.CORRUPTED_HUNLLEF_9038
-    };
-
-    private static final int[] ATTACK_PROJECTILE_IDS = {
-        // Intentionally empty until debug fixtures verify ranged/magic projectile IDs and attack cadence.
-    };
-
-    private static final int[] TORNADO_NPC_OR_GRAPHIC_IDS = {
-        // Intentionally empty until debug fixtures verify tornado NPC/graphic IDs.
-    };
+        9038); // RuneLite NpcID.CORRUPTED_HUNLLEF_9038
 
     private HunllefIds()
     {
     }
 
-    public static int[] crystallineHunllefNpcIds()
+    public static boolean isHunllef(int npcId)
     {
-        return CRYSTALLINE_HUNLLEF_NPC_IDS.clone();
+        return isCrystallineHunllef(npcId) || isCorruptedHunllef(npcId);
     }
 
-    public static int[] corruptedHunllefNpcIds()
+    public static boolean isCrystallineHunllef(int npcId)
     {
-        return CORRUPTED_HUNLLEF_NPC_IDS.clone();
+        return CRYSTALLINE_HUNLLEF_NPC_IDS.contains(npcId);
     }
 
-    public static int[] attackProjectileIds()
+    public static boolean isCorruptedHunllef(int npcId)
     {
-        return ATTACK_PROJECTILE_IDS.clone();
-    }
-
-    public static int[] tornadoNpcOrGraphicIds()
-    {
-        return TORNADO_NPC_OR_GRAPHIC_IDS.clone();
+        return CORRUPTED_HUNLLEF_NPC_IDS.contains(npcId);
     }
 }
